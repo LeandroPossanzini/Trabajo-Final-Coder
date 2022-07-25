a79cfc033d5053dc2a65312012419af6e8ebff04
Leandro Possanzini <leandropossanzini@gmail.com>
2022-07-25 12:59:17 -0300

    Agregue el navbar, header y footer

--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
-import { HeaderComponent } from '../core/components/header/header.component';
-import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
-import { TitleComponent } from '../core/components/title/title.component';
+import { HeaderComponent } from './../core/components/header/header.component';
+import { SidebarComponent } from './../core/components/sidebar/sidebar.component';
+import { TitleComponent } from './../core/components/title/title.component';
 
 
 @NgModule({
